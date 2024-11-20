0220673c54b73cc396090436f8d97b79f0cbd4c3
Stephen Gruzin <stephengrzn@gmail.com>
2024-11-19 19:48:05 -0800

    Working on the static folder and setting up the login page.

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/filesystem"
+	"github.com/gofiber/template/html/v2"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	engine := html.New("./templates", ".html")
+
+	app := fiber.New(fiber.Config{
+		Views: engine,
+	})
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		fmt.Println(c.Cookies("password"))
+		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/login", func(c *fiber.Ctx) error {
+		return c.Render("login", fiber.Map{
+			"Title": "Hello, World! Test",
+		})
+	})
+
+	app.Use("/static", filesystem.New(filesystem.Config{
+		Root: http.Dir("./assets"),
+	}))
+
+	log.Fatal(app.Listen(":3000"))
 }
