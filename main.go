1a09695c32a99f95b84067a33f6be201c8c9953e
Stephen Gruzin <stephengrzn@gmail.com>
2024-11-19 21:59:52 -0800

    Setup basic login and logout methods

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/Stephen10121/planningcenterbackend/initializers"
 	"github.com/gofiber/fiber/v2"
@@ -21,15 +22,39 @@ func main() {
 	app.Get("/", func(c *fiber.Ctx) error {
 		password := c.Cookies("password")
 		if password != initializers.Password {
-			c.Redirect("/login")
+			return c.Redirect("/login")
 		}
 		return c.SendString("Hello, World!")
 	})
 
 	app.Get("/login", func(c *fiber.Ctx) error {
-		return c.Render("login", fiber.Map{
-			"Title": "Hello, World! Test",
-		})
+		password := c.Cookies("password")
+		if password == initializers.Password {
+			return c.Redirect("/")
+		}
+		return c.Render("login", fiber.Map{})
+	})
+
+	app.Post("/login", func(c *fiber.Ctx) error {
+		password := c.FormValue("password")
+		if password != initializers.Password {
+			return c.Render("login", fiber.Map{
+				"Error": "Invalid Password!",
+			})
+		}
+
+		cookie := new(fiber.Cookie)
+		cookie.Name = "password"
+		cookie.Value = password
+		cookie.Expires = time.Now().Add(24 * time.Hour)
+
+		c.Cookie(cookie)
+		return c.Redirect("/")
+	})
+
+	app.Get("/logout", func(c *fiber.Ctx) error {
+		c.ClearCookie("password")
+		return c.Redirect("/login")
 	})
 
 	app.Use("/static", filesystem.New(filesystem.Config{
