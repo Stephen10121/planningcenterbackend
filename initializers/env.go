6319bc98f280345d0bc83f89524e6c5a1856267d
Stephen Gruzin <stephengrzn@gmail.com>
2024-11-23 15:19:36 -0800

    Trying new way of passing env vars

--- initializers/env.go
+++ initializers/env.go
@@ -2,17 +2,19 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 var Password string
 var Credentials string
+var ResourcesJsonFilePath string
 
 func SetupEnv() {
 	envFile, _ := godotenv.Read(".env")
 
-	calPassword := envFile["CAL_PASSWORD"]
+	calPassword := os.Getenv("CAL_PASSWORD")
 
 	if len(calPassword) != 0 {
 		Password = calPassword
@@ -26,5 +28,12 @@ func SetupEnv() {
 		Credentials = cred
 	} else {
 		log.Fatalln("CREDENTIALS env variable is not set.")
+	}
+
+	resFile := envFile["RESOURCESFILE"]
+	if len(resFile) != 0 {
+		ResourcesJsonFilePath = resFile
+	} else {
+		log.Fatalln("RESOURCESFILE env variable is not set.")
 	}
 }
